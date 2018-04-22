7e10d55d43dc75cdc69b7365e188aa3afe120d29
Dominic Scheirlinck <dominic@vitaler.co>
2018-04-23 11:08:05 +1200

    Added basics of commands

---
+++ lib/metadata.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function plugin_get_metadata() {
+  local key="metadata-env-plugin-$1"
+  plugin_prompt buildkite-agent meta-data get "$key"
+  buildkite-agent meta-data get "$key" || (
+    echo "~~~ Failed to get metadata $key (exit $?)" >&2
+    return 1
+  )
+}
+
+function plugin_set_metadata() {
+  local key="metadata-env-plugin-$1"
+  local value="$2"
+  plugin_prompt_and_must_run buildkite-agent meta-data set "$key" "$value"
+}
