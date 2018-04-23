7ac411fc51bd3335a943828094be385fcd906981
Dominic Scheirlinck <dominic@vitaler.co>
2018-04-23 13:19:37 +1200

    Just use env- as metadata prefix

--- lib/metadata.bash
+++ lib/metadata.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function plugin_get_metadata() {
-  local key="metadata-env-plugin-$1"
+  local key="env-$1"
   plugin_prompt buildkite-agent meta-data get "$key"
   buildkite-agent meta-data get "$key" || (
     echo "~~~ Failed to get metadata $key (exit $?)" >&2
@@ -10,7 +10,7 @@ function plugin_get_metadata() {
 }
 
 function plugin_set_metadata() {
-  local key="metadata-env-plugin-$1"
+  local key="env-$1"
   local value="$2"
   plugin_prompt_and_must_run buildkite-agent meta-data set "$key" "$value"
 }
